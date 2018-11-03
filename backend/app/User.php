185b9fd946bedcaf1707d6eba1d78296eec53f9d
YoshijiFujiwara <yfbiology2@gmail.com>
2018-11-04 08:21:35 +0900

    JWTのインストール

--- backend/app/User.php
+++ backend/app/User.php
@@ -5,26 +5,30 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // JWTに必要
+    public function getJWTIdentifier()
+    {
+        // ユーザーのプライマリーキーを返す
+        return $this->getKey();
+    }
+
+    // JWTに必要
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
